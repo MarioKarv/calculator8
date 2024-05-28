d8c70610ab515599c24167c92ce3b5ec82fa3434
MarioKarv <mariook0204@gmail.com>
2024-05-28 21:26:12 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import "./App.css";
 
-function App() {
+const Calculator = () => {
+  const inputElement = useRef(null);
+  const [total, setTotal] = useState(0);
+
+  const handleCalculation = (operation) => (e) => {
+    e.preventDefault();
+    const inputValue = Number(inputElement.current.value);
+    setTotal((prevTotal) => {
+      switch (operation) {
+        case "add":
+          return prevTotal + inputValue;
+        case "subtract":
+          return prevTotal - inputValue;
+        case "multiply":
+          return prevTotal * inputValue;
+        case "divide":
+          return prevTotal / inputValue;
+        default:
+          return prevTotal;
+      }
+    });
+  };
+
+  const handleResetInput = (e) => {
+    e.preventDefault();
+    inputElement.current.value = "";
+  };
+
+  const handleResetTotal = (e) => {
+    e.preventDefault();
+    setTotal(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <h1>Simple React Calculator</h1>
+      <form>
+        <p className="result">{total}</p>
+        <input ref={inputElement} type="number" placeholder="Enter a number" />
+        <div className="button-group">
+          <button onClick={handleCalculation("add")}>Add</button>
+          <button onClick={handleCalculation("subtract")}>Subtract</button>
+          <button onClick={handleCalculation("multiply")}>Multiply</button>
+          <button onClick={handleCalculation("divide")}>Divide</button>
+        </div>
+        <div className="reset-group">
+          <button onClick={handleResetInput}>Reset Input</button>
+          <button onClick={handleResetTotal}>Reset Total</button>
+        </div>
+      </form>
     </div>
   );
-}
+};
 
-export default App;
+export default Calculator;
